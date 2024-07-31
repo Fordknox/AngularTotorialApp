162351c05333c76d7e51e2cfdd6244f2f86466e0
Fordknox <henry.van.der.lith@gmail.com>
2024-07-31 19:08:15 +0200

    Getting Started Angular Totorial Done

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { products } from '../products';
+import { ProductAlertsComponent } from '../product-alerts/product-alerts.component';
 
 @Component({
   selector: 'app-product-list',
@@ -12,6 +13,9 @@ export class ProductListComponent {
 
   share() {
     window.alert('The product has been shared!');
+  }
+  onNotify(){
+    window.alert("you will be notified when the product goes on sale")
   }
 }
 
