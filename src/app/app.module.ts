37232eae8f7d2b278d015462f4a379363e56c8f6
Fordknox <henry.van.der.lith@gmail.com>
2024-08-02 22:04:44 +0200

    remove TopBar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
@@ -35,7 +34,6 @@ import { CustomCurrencyModule } from '../custom-currency.module';
 ],
   declarations: [
     AppComponent,
-    TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
